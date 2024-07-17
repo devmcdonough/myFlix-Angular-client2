4c97bfe085996bda7eee969cc5504aa91d6cd21c
devmcdonough <devmcdonough@gmail.com>
2024-07-17 15:28:24 -0400

    Added jsdoc comments

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -3,23 +3,43 @@ import { UserLoginFormComponent } from '../user-login-form/user-login-form.compo
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
+/**
+ * Component for the welcome page.
+ * @component
+ */
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
+  /**
+   * Creates an instance of WelcomePageComponent.
+   * @param {MatDialog} dialog - Angular Material Dialog service.
+   */
   constructor(public dialog: MatDialog) { }
+
+  /**
+   * Angular's OnInit lifecycle hook.
+   */
   ngOnInit(): void {
   }
+
+  /**
+   * Opens the user registration dialog.
+   */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       width: '280px'
     });
   }
+
+  /**
+   * Opens the user login dialog.
+   */
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
-    })
+    });
   }
 }
